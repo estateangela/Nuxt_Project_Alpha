86f9c0c87b7e3f4c6c2aeba3eee3d32137d7883e
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-02 19:52:12 +0800

    initial

---
+++ front-page/conponents/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/about">about us</nuxt-link></li>
+          <li><nuxt-link to="/contact">contact</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+  }
+  nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+  }
+  nav a {
+    color: white;
+    text-decoration: none;
+  }
+  </style>
