e6ba3d089c8a580efef669674404932aa4e04967
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-03 23:03:33 +0800

    !!

--- front-page/app.vue
+++ front-page/app.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <Header />
-    <!-- 其他内容! -->
-    <NuxtPage />
-    <Footer />
+    
   </div>
 </template>
--- front-page/pages/index.vue
+++ front-page/pages/index.vue
@@ -1,6 +1,11 @@
 <template>
+    <Header />
+    <!-- 其他内容! -->
+   
+   
     <div>
       <h1>Welcome to My Website</h1>
       <p>This is the home page content.</p>
     </div>
+    <Footer />
   </template>
