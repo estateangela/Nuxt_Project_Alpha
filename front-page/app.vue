f2abf0f58a7152f11dde9168f4b6a1d5e82272c7
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-03 22:27:28 +0800

    fix app.vue

--- front-page/app.vue
+++ front-page/app.vue
@@ -2,18 +2,6 @@
   <div id="app">
     <Header />
     <!-- 其他内容 -->
-    <router-view></router-view>
+    <NuxtPage />
   </div>
 </template>
-
-<script>
-import Header from 'front-page/components/Header.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Header
-  }
-}
-</script>
-
